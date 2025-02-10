41b6920c049eaf244be81fa12dcfdd15da1397e7
Sankalp <sankalpbisan07@gmail.com>
2025-02-10 17:07:44 +0530

    Data Transformation Completed (data_transformation.py) along with scripting utils.py and mkdir - artifact added

---
+++ src/components/data_transformation.py
@@ -0,0 +1,114 @@
+import sys
+from dataclasses import dataclass
+import os
+from src.utils import save_object
+
+import pandas as pd
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.logger import logging
+from src.exception import CustomException
+
+from src.utils import proj_dir_path
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join(proj_dir_path+'artifact',"preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            numerical_columns = ["writing_score","reading_score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "lunch",
+                "test_preparation_course",
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy="median")),
+                    ("scaler",StandardScaler(with_mean=False))
+                ]
+            )
+            logging.info("Numerical Features Pipeline transformed using standard-scaler")
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder",OneHotEncoder()),
+                    ("scaler",StandardScaler(with_mean=False))
+                ]
+            )
+            logging.info("Categorical Features Pipeline transformed using one_hot_encoding")
+
+            #Combine two pipeline / Transforming num and cat Columns together
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline",num_pipeline,numerical_columns),
+                    ("cat_pipeline",cat_pipeline,categorical_columns)
+                ]
+            )
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            logging.info("Read Train-Test data successfully")
+
+            logging.info("Getting preprocessing object")
+            preprocessor_obj =self.get_data_transformer_object()
+
+            '''
+            #For debugging...
+            logging.info(f"preprocessor object type:- {type(preprocessor_obj)}")
+            '''
+
+            target_column_name = "math_score"
+            numerical_columns = ["writing_score", "reading_score"]
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info("Applying Transformation to Train & Test Data")
+
+            input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessor_obj.fit_transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info("Transformation applied to train and test data, using preprocessor objet")
+
+            save_object(
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessor_obj
+            )
+
+            logging.info("Stored as pickle file:- preprocessor object from data transformation")
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
--- src/exception.py
+++ src/exception.py
@@ -1,19 +1,16 @@
-import sys
 from src.logger import logging
+import sys
 
 def error_message_detail(error,error_detail:sys):
     '''
     This function is designed to explicitly handle exception
-    :param error:
-    :param error_detail:
-    :return:
     '''
     _,_,exc_tb = error_detail.exc_info() #the 1st and 2nd variable are not much helpful
         # 3rd variable stores all the details related to error,
         # like what the error is and on which it has occurred etc.....
     file_name =exc_tb.tb_frame.f_code.co_filename
     # This gives the filename
-    error_message = "Error occurred in python file name [{0}] line number [{}] error message [{2}]".format(
+    error_message = "Error occurred in python file name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno,str(error))
 
     return error_message
@@ -22,7 +19,7 @@ def error_message_detail(error,error_detail:sys):
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message_detail(error_message,error_detail=error_detail)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
